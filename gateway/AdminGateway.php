1263161291325f2f16b256f226f9f3967291812b
Becos <becos19@mail.ru>
2022-01-21 22:21:36 +0500

    v21.01.22 - чистка

--- gateway/AdminGateway.php
+++ gateway/AdminGateway.php
@@ -9,43 +9,6 @@ class AdminGateway
 		$this->con = $con;
 	}
 
-	public function IdPasswordCheck(string $id, string $password)
-	{
-		try {
-			$query = "SELECT Count(*) where login=:id AND password =:password ";
-			$this->con->executeQuery($query, array(
-				'id'  => array($id, PDO::PARAM_STR),
-				'password' => array($password, PDO::PARAM_STR)
-			));
-			$res = $this->con->getResults();
-			return $res;
-			if ($res != 1)
-				return true;
-			return false;
-		}
-		catch (PDOException $e) {
-			$this->myError($e);
-		}
-	}
-
-	public function AdminCheck(Admin $ad)
-	{
-		try {
-			$query = "SELECT COUNT(*) WHERE login=:id AND password =:password ";
-			$this->con->executeQuery($query, array(
-				':id'  => array($ad->getlogin(), PDO::PARAM_STR),
-				':password' => array($ad->getPassword(), PDO::PARAM_STR)
-			));
-			$res = $this->con->getResults();
-			if ($res != 1)
-				return true;
-			return false;
-		}
-		catch (PDOException $e) {
-			$this->myError($e);
-		}
-	}
-
 	public function checkAdmin(string $login, string $password)
 	{
 		try {
